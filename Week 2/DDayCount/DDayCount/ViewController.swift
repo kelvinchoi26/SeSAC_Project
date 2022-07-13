d64fa68847548ecda32254fa1795f6b088cc9281
kelvinchoi26 <kelvinchoi@naver.com>
2022-07-13 16:50:13 +0900

    기념일 계산기 UI 구현

---
+++ Week 2/DDayCount/DDayCount/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DDayCount
+//
+//  Created by 최형민 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // 최근 날짜 순으로 imagview/label에 데이터 띄우기
+
+
+}
+
--- Week 2/SeSACWeek2_문법연습.playground/Contents.swift
+++ Week 2/SeSACWeek2_문법연습.playground/Contents.swift
@@ -16,3 +16,43 @@ func example() -> String {
 }
 
 example()
+
+class Monster {
+    var clothes = "Orange + Christmas"
+    var speed = 5
+    var power = 20
+    var expoint: Double = 5000
+    
+    func attack() {
+        print("공격!")
+    }
+}
+
+var easyMonster = Monster()
+easyMonster.clothes
+easyMonster.power
+
+easyMonster.attack()
+easyMonster.attack()
+
+// 겹치는 기능들이 많으니 Monster 클래스로부터 상속
+class BossMonster: Monster {
+    var levelLimit = 500
+    
+    func bossAttack() {
+        print("스페셜 공격!")
+    }
+    // 재정의
+    override func attack() {
+        // 슈퍼클래스의 함수도 실행해주고 싶을 때 super로 불러옴
+        super.attack()
+        print("오버라이드 공격!")
+    }
+}
+
+var finalBoss = BossMonster()
+finalBoss.clothes
+
+// struct는 상속이 불가능하다
+// 구조체는 값 타입, 클래스는 참조 타입
+
